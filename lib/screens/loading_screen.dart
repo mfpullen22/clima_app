a20fdd5937ebff86279e3e4649453ecdc15f1eb1
Matthew Pullen <matthew.pullen@gmail.com>
2021-04-05 00:49:42 -0500

    Added API call to OpenWeather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:clima/services/location.dart";
+import "package:http/http.dart" as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,7 +8,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -21,9 +21,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22"));
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    );
+    getData();
+    return Scaffold();
   }
 }
